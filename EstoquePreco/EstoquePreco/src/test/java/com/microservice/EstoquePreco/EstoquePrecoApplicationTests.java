8a77184afb804843592a1c3882f3a36ea8e2b815
RodrigoBritoGit <Rodrigodossantosbrito@hotmail.com>
2024-07-05 10:53:59 -0300

    Configuração inicial imagem docker RabbitMQ, criação do projeto Java Spring Boot e primeiras configurações

---
+++ EstoquePreco/EstoquePreco/src/test/java/com/microservice/EstoquePreco/EstoquePrecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.EstoquePreco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoquePrecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
